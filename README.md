4bf3256bdd0ddf26738c59ae7e243d0967fe74a2
Jessica Jimenez <jjimenez174@yahoo.com>
2024-05-25 21:49:21 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Text-Editor
+
+## Description
+
+
+[Link to Demo]()
+
+## Table of Content
+[Installation](#installation)  
+[Usage](#usage)  
+[Contribution](#contribution)  
+[Test](#test)  
+[Questions](#questions) 
+
+## Installation
+* Clone the repositoy
+* Open the file in VS code or any source code editor
+* Open terminal install 
+    * <b>npm i</b>
+* Change the databse login in .env file 
+* run <b> mysql -u root -p</b>  
+* <b>SOURCE db/schema.sql</b>
+* <b>npm start</b> to run the application
+
+All set to run the app. 
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+For more information, visit [License Details](https://opensource.org/license/MIT)
+
+## Questions
+
+Contact me at: [jjimenez174](https://github.com/jjimenez174)
+
+## Deploy Application
+
+[Link](https://text-editor-ift1.onrender.com/)
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,13 @@
+// Logic for installing the PWA
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// An event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// A click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
@@ -18,7 +18,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// An handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
